c21ca0d215f7ad88d3ecdbd3d7630d98c3f836c9
TRASMIT <vasukiran864@gmail.com>
2025-03-16 22:28:28 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,10 @@
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBOIYbsZq8J-N2e-DE9lQtqBplmJGGyU2Y",
   authDomain: "elvenail.firebaseapp.com",
   projectId: "elvenail",
   storageBucket: "elvenail.firebasestorage.app",
   messagingSenderId: "157530111056",
-  appId: "1:157530111056:web:a430f8a8ee78b6fcf16555",
-  measurementId: "G-9GK31THNTV"
+  appId: "1:157530111056:web:a430f8a8ee78b6fcf16555"
 };
 
 // Initialize Firebase
